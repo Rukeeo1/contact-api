46a533748878253cc345dbf5bcfc709fc36b639d
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-07-13 01:46:09 +0100

    delete functionality now working and json is being updated....i need to clean up my code and abstract the fswrite into a function

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,7 @@ import uuid from 'uuid';
 import joi from '@hapi/joi';
 import fs from 'fs';
 
-// import  contact from '../../data/contact'
 const contacts = require('../../data/contact');
-
 const router = Router();
 
 /* GET home page. */
@@ -13,9 +11,10 @@ router.get('/', function(_req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/*
+
 function writeToJson(parameter:object[]){
-  fs.writeFile('./src/routes/index.json', parameter, 'utf8', function(
+  let toJson = JSON.stringify(parameter)
+  fs.writeFile('/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',toJson, 'utf8', function(
     err: Error,
     data: object[]
   ): any {
@@ -25,41 +24,41 @@ function writeToJson(parameter:object[]){
   });
 }
 
-*/
-
-//get everyone in the contact list...
-// router.get('/api/contacts', (_req, res: any) => {
-//   console.log('hello');
-//   res.json(contact);
-// });
-
 router.get('/api/contacts', (_req, res: any) => {
-  fs.readFile('./src/routes/index.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    let contacts = JSON.parse(data);
-    res.send(contacts);
-  });
+  fs.readFile(
+    '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+    'utf8',
+    function(err, data) {
+      if (err) throw err;
+      let contacts = JSON.parse(data);
+      res.send(contacts);
+    }
+  );
 });
 
 router.get('/api/contacts/:id', (req, res) => {
-  fs.readFile('./src/routes/index.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    let contacts = JSON.parse(data);
-    console.log(contacts);
-    const found = contacts.some(
-      (contact: { id: string }) => contact.id === req.params.id
-    );
+  fs.readFile(
+    '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+    'utf8',
+    function(err, data) {
+      if (err) throw err;
+      let contacts = JSON.parse(data);
 
-    if (found) {
-      res.json(
-        contacts.filter(
-          (contact: { id: string }) => contact.id === req.params.id
-        )
+      const found = contacts.some(
+        (contact: { id: string }) => contact.id === req.params.id
       );
-    } else {
-      res.status(400).json({ msg: 'Member not Found' });
+
+      if (found) {
+        res.json(
+          contacts.filter(
+            (contact: { id: string }) => contact.id === req.params.id
+          )
+        );
+      } else {
+        res.status(400).json({ msg: 'Member not Found' });
+      }
     }
-  });
+  );
 });
 //my scheman validates name...
 //address...
@@ -117,62 +116,104 @@ router.post('/api/contacts', (req, res) => {
   //let updatedContactList = JSON.stringify([...contacts, newContact]);
   let updatedContactList = JSON.stringify(contacts);
 
-  fs.writeFile('./src/routes/index.json', updatedContactList, 'utf8', function(
-    err: Error,
-    data: object[]
-  ): any {
-    if (err) throw err;
+  fs.writeFile(
+    '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+    updatedContactList,
+    'utf8',
+    function(err: Error, data: object[]): any {
+      if (err) throw err;
 
-    console.log(data);
-  });
+      console.log(data);
+    }
+  );
+  
 });
 
 router.put('/api/contacts/:id', (req, res) => {
-  fs.readFile('./src/routes/index.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    let contacts = JSON.parse(data);
+  fs.readFile(
+    '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+    'utf8',
+    function(err, data) {
+      if (err) throw err;
+      let contacts = JSON.parse(data);
 
-    const found = contacts.some(
-      (contact: { id: string }) => contact.id === req.params.id
-    ); //check to see if the nmember exist
+      const found = contacts.some(
+        (contact: { id: string }) => contact.id === req.params.id
+      );
 
-    if (found) {
-      const updatedMember = req.body;
+      if (found) {
+        const updatedMember = req.body;
 
-      contacts.forEach((contact )=> {
-        if (contact.id === req.params.id) {
-          contact.name = updatedMember.name ? updatedMember.name : contact.name;
-          contact.email = updatedMember.email
-            ? updatedMember.email
-            : contact.email;
-          contact.phone = updatedMember.phone
-            ? updatedMember.phone
-            : contact.phone;
-          contact.company = updatedMember.company
-            ? updatedMember.company
-            : contact.company;
-          contact.isBlocked = updatedMember.isBlocked
-            ? updatedMember.isBlocked
-            : contact.isBlocked;
+        contacts.forEach((contact: any) => {
+          if (contact.id === req.params.id) {
+            contact.name = updatedMember.name
+              ? updatedMember.name
+              : contact.name;
+            contact.email = updatedMember.email
+              ? updatedMember.email
+              : contact.email;
+            contact.phone = updatedMember.phone
+              ? updatedMember.phone
+              : contact.phone;
+            contact.company = updatedMember.company
+              ? updatedMember.company
+              : contact.company;
+            contact.isBlocked = updatedMember.isBlocked
+              ? updatedMember.isBlocked
+              : contact.isBlocked;
 
-          res.json({ msg: 'contact was updated', contact });
+            res.json({ msg: 'contact was updated', contact });
 
-          fs.writeFile('./src/routes/index.json', contacts, 'utf8', function(
-            err: Error,
-            data: object[]
-          ): any {
-            if (err) throw err;
+            fs.writeFile(
+              '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+              JSON.stringify(contacts),
+              'utf8',
+              function(err: Error, data: object[]): any {
+                if (err) throw err;
 
-            console.log(data);
-          });
-        }
-      });
-    } else {
-      res.status(400).json({ msg: 'Member not Found' });
+                console.log(data);
+              }
+            );
+          }
+        });
+      } else {
+        res.status(400).json({ msg: 'Member not Found' });
+      }
     }
-  });
+  );
 });
 
+router.delete('/api/contacts/:id', (req, res) => {
+  fs.readFile(
+    '/Users/rukeeo1/Downloads/Compressed/express-starter/data/contact.json',
+    'utf8',
+    function(err, data) {
+      if (err) throw err;
+      let contacts = JSON.parse(data);
+     
+      const found = contacts.some(
+        (contact: { id: string }) => contact.id === req.params.id
+      );
+
+      if (found) {
+
+        const contactListAfterDelete  =    contacts.filter(
+            (contact: { id: string }) => contact.id !== req.params.id
+           )
+        res.json(
+          // contacts.filter(
+          //   (contact: { id: string }) => contact.id !== req.params.id
+          // )
+          contactListAfterDelete 
+        );
+        writeToJson(contactListAfterDelete)
+
+      } else {
+        res.status(400).json({ msg: 'Member not Found' });
+      }
+    }
+  );
+});
 //i see a put request,
 //but i dont' see a delete request...
 //how do i get a
